98e3c5ef21b1372357ba5d28cfbd8b13f4b803c8
vaughnjones-marcus <vaughnjones.marcus@gmail.com>
2023-11-29 16:58:34 -0600

    Style homepage

--- rpg/rpg/urls.py
+++ rpg/rpg/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
+from . import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('leveling/', include('leveling.urls')),
+    path('homepage/', index.homepage),
 ]
